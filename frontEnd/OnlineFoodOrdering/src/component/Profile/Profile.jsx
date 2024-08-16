c5e03f7fc2e838f3cf8ff5c1d87993551b940a4e
jarvisthedev <johnsonmakings@gmail.com>
2024-08-16 23:50:00 +0300

    Initiatiated  Addition of ProfilePage

---
+++ frontEnd/OnlineFoodOrdering/src/component/Profile/Profile.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ProfileNavigation from "./ProfileNavigation";
+
+const Profile = () => {
+  return (
+    <div>
+      <p>Profile</p>
+      <ProfileNavigation />
+    </div>
+  );
+};
+
+export default Profile;
